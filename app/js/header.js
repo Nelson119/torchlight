e6dbdfcbacb96bff6436f2c694c992c07479ac78
nelson <nelson119@outlook.com>
2016-12-17 21:15:40 +0800

    news 1st v

--- app/js/header.js
+++ app/js/header.js
@@ -7,17 +7,31 @@
 /*global app, $ */
 app.partial.header = function(){
 	
-	var navShowSubTimeout = 3;
-
+	var navShowSubTimeout = 0.25;
 
 	var navShowSubTimeoutTick = 0;
-	$('header nav').on('mousemove', function(e){
-		var nav = $('header nav');
+
+	var nav = $('header');
+	$('header nav >aside').on('mousemove', function(e){
+		clearTimeout(navShowSubTimeoutTick);
 		nav.addClass('nav-show-sub');
+	});
+	$('header').on('mousemove', function(e){
+		clearTimeout(navShowSubTimeoutTick);
+	});
+	$('header').on('mouseout', function(e){
 		clearTimeout(navShowSubTimeoutTick);
 		navShowSubTimeoutTick = setTimeout(function(){
 			nav.removeClass('nav-show-sub');
-		});
-		
+		}, navShowSubTimeout * 1000);
+	});
+
+	$(window).on('scroll', function(){
+		var scrollTop = $(window).scrollTop();
+		if(scrollTop > 30){
+			$('header').addClass('shrink');
+		}else{
+			$('header').removeClass('shrink');
+		}
 	});
 };	
